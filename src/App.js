3a4ce853a0c885209097e4cf80111d0d1341d299
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-02-25 11:32:14 +0600

    8. Pass object to components and access object added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  {/* 7. Pass dynamic data to components, props in react */}
+  {/* 7. Pass dynamic data to components, props in react */ }
   const nayoks = ['Azmol', 'Manna'];
+
+  // 8. Pass object to components and access object
+  const products = [{ name: 'Photoshop', price: '$90.99' }, { name: 'Illustrator', price: '$85.99' }
+  ];
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      {/* 8. Pass object to components and access object */}
+        {/* <Prodect name={products[0].name} price = {products[0].price}></Prodect> */}
+        <Prodect product={products[0]}></Prodect>
+        <Prodect product={products[1]}></Prodect>
+
 
         {/* 7. Pass dynamic data to components, props in react */}
         <Person nayok="Rebel" Naika="Mousumi"></Person>
@@ -21,6 +27,31 @@ function App() {
     </div>
   );
 }
+
+// 8. Pass object to components and access object
+function Prodect(props) {
+  const ProdectStyle = {
+    border: "1px solid gray",
+    borderRadius: '5px',
+    backgroundColor: 'lightorange',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  // array desttucturing
+  const {name, price} = props.product;
+
+  return (
+    <div style={ProdectStyle}>
+      {/* <h3>{props.product.name}</h3> */}
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
+
 
 // 7. Pass dynamic data to components, props in react
 function Person(props) {
